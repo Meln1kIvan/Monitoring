ba09bad6e9b2dc949dea15256f4ddfe022d5e67d
melnik_ivan2005@mail.ru <melnik_ivan2005@mail.ru>
2024-10-17 11:39:39 +0300

    InitialCommit

---
+++ Monitoring/Models/ViewMaintenanceCostAnalysis.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monitoring.Models;
+
+public partial class ViewMaintenanceCostAnalysis
+{
+    public string? EquipmentName { get; set; }
+
+    public decimal? TotalMaintenanceCost { get; set; }
+}
---
+++ Monitoring/Models/ViewUnscheduledMaintenanceFrequency.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monitoring.Models;
+
+public partial class ViewUnscheduledMaintenanceFrequency
+{
+    public string? EquipmentName { get; set; }
+
+    public int? UnscheduledMaintenanceCount { get; set; }
+}
